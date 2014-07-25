ef82624358eeb1763e50709bd922be3b46273773
Chunyang Xu <xuchunyang56@gmail.com>
2014-07-26 03:00:02 +0800

    add library deps
    * 有个 warning 在 configure.ac 中

--- src/main.c
+++ src/main.c
@@ -1,11 +1,49 @@
 #include <config.h>
 #include <stdio.h>
+#include <glib.h>
 
-int
-main (int argc, char *argv[])
+#include <wavpack/wavpack.h>
+
+#define MAXLEN 1024
+void  read_wavpack_file (gchar *filename)
 {
-  puts ("Hello World!");
-  puts ("This is " PACKAGE_STRING ".");
+    WavpackContext *wpc;
+    gchar *field;
+    guint length;
 
-  return 0;
+    int open_flags = OPEN_TAGS;
+
+    wpc = WavpackOpenFileInput(filename, NULL, open_flags, 0);
+
+    if ( wpc == NULL ) {
+        fprintf (stderr, "Error: open %s as wavpack failed\n", filename);
+        return;
+    }
+
+    /* title */
+    field = g_malloc0(sizeof(char) * MAXLEN);
+    length = WavpackGetTagItem(wpc, "title", field, MAXLEN);
+    if ( length > 0 )
+        printf ("title: %s\n", field);
+
+    g_free (field);
+    WavpackCloseFile(wpc);
 }
+
+int main (int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        fprintf(stderr, "Usage:: %s wavpack_file\n", argv[0]);
+        return 1;
+    }
+    else
+    {
+        while (--argc)
+            read_wavpack_file(*++argv);
+    }
+
+    return 0;
+}
+
+
